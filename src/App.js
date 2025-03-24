2e62175c3e50a0d8e378a3709469e8682cb49bf7
Jarrett Ferris <ferriswrites@gmail.com>
2025-03-23 19:58:56 -0700

    Library ecommerce website created with React first push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from 'react';
+import Footer from "./components/Footer";
+import Nav from "./components/Nav";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Books from "./pages/Books";
+import { books } from './data.js';
+import BookInfo from "./pages/BookInfo.jsx";
+import Cart from "./pages/Cart.jsx";
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  function addToCart(book) {
+    const dupeItem = cart.find(item => +item.id === +book.id)
+    if (dupeItem) {
+      setCart(cart.map(item => {
+        if (item.id === dupeItem.id) {
+          return {
+            ...item,
+            quantity: item.quantity + 1,
+          }
+        }
+        else {
+          return item
+        }
+      }))
+    }
+    else {
+      setCart([...cart, {...book, quantity: 1}])
+    }
+  }
+
+  function changeQuantity(book, quantity, price) {
+    setCart(cart.map((item) => 
+      item.id === book.id ? 
+         {
+          ...item,
+          quantity: +quantity,
+        }
+        : item
+      )
+    );
+  }
+
+  function removeItem(item) {
+    setCart(cart.filter((book) => book.id !== item.id));
+  }
+
+  function numberOfItems() {
+    let counter = 0;
+    cart.forEach((item) => {
+      counter += item.quantity;
+    });
+    return counter;
+  }
+
+  useEffect(() => {
+    
+  }, [cart])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Nav numberOfItems={numberOfItems()} />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/books" exact element={<Books books={books} /> } />
+          <Route path="/books/:id" element={<BookInfo books={books} addToCart={addToCart}/> } />
+          <Route path="/cart" element={<Cart books={books} cart={cart} changeQuantity={changeQuantity} removeItem={removeItem} />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
